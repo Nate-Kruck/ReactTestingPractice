d87340ed28160726e960d64ac624d1f74c0736fb
Nathan Kruckenberg <natekruck1990@gmail.com>
2022-02-01 10:54:04 -0600

    added comment box component that with event handlers

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,6 +1,39 @@
 /* eslint-disable import/no-anonymous-default-export */
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-    return <div>Box for Comment</div>
+// add class based component that is responsible for watching input for a
+// user
+
+class CommentBox extends Component {
+    state = { comment: '' };
+
+    handleChange = event => {
+        this.setState({ comment: event.target.value })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        // TODO Call an action creator
+        // And save the comment
+        this.setState({ comment: '' });
+    }
+
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <h4>Add a Comment</h4>
+
+                {/* add event handler in text area to change state */}
+                <textarea onChange={this.handleChange} value={this.state.comment} />
+
+                <div>
+                    <button>Submit Comment</button>
+                </div>
+            </form>
+        );
+    }
 }
+
+export default CommentBox;
